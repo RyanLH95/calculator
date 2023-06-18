97af19eabae3c051e0d2123e50ec10e82a99f268
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-06-18 13:15:26 +0100

    create a class with constructor for previousOperandTextElement and currentOperandTextElement

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+/*script.js*/
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement)
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
