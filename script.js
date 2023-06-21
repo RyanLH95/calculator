e731dcc0791eab79adca327da80efd387334fbd8
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-06-21 10:11:19 +0100

    create addEventListener to call in on the compute function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 /*script.js*/
 
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement)
+    constructor(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement
       this.currentOperandTextElement = currentOperandTextElement
       this.clear()
+    }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -15,40 +16,49 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
-clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
-}
-
-delete() {
-}
-
-appendNumber(number) {
-}
-
-chooseOperation(operation) {
-}
-
-compute() {
-}
-
-updateDisplay() {
-}
 
 const calculator = new Calculator(previousOperandTextElement, 
     currentOperandTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-       calculator.appendNumber(button.innerText)
-       calculator.updateDisplay(this.currentOperandTextElement.innerText
-         = this.currentOperand)
-    })
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+   })
 })
 
-appendNumber(number) {
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+clear() ;{
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+};
+
+delete(value) ;{
+}
+
+appendNumber(number) ;{
     if (number === '.' && this.currentOperand.includes('.')) return
     this.currentOperand = this.currentOperand.toString() + number.toString()
 }
 
+chooseOperation(operation) ;{
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '') {
+        this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
+}
+
+compute() ;{
+}
+
+updateDisplay() ;{
+    this.currentOperandTextElement.innerText = this.currentOperand
+}
